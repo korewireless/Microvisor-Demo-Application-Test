cd8c1812b36b70bc00f67f90e8105a98d5201e62
smittytone <tsmith@korewireless.com>
2023-11-20 16:06:01 +0000

    Add GH action

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,7 @@ cd $(dirname $0)
 
 [ -d build ] && rm -rf build
 
-# Place these here so dependencies are checked
-# even if the take the BUILD_ONLY path (GitHub Action)
+# Build and bundle the app test code
 cmake -S . -B build
 cmake --build build
 twilio microvisor:apps:bundle build/app/mv-app-test.bin build/app/mv-app-test.zip
